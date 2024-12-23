004005fb7ec7379c75e625a811910c79b73f1bf1
Vishesh Patel <81861249+vishesh27111@users.noreply.github.com>
2024-12-22 21:57:42 -0400

    Add files via upload

--- flutter_app/lib/pages/detection.dart
+++ flutter_app/lib/pages/detection.dart
@@ -23,27 +23,10 @@ class Detection extends StatefulWidget {
 }
 
 class _DetectionState extends State<Detection> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _fadeAnimation;
-
-  bool _showActions = false; // To control when to show the actions
 
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(duration: const Duration(seconds: 7), vsync: this);
-    _fadeAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
-    _controller.forward();
-
-    Future.delayed(const Duration(seconds: 9), () {
-      setState(() {
-        _showActions = true;
-      });
-    });
-
     _initializeAsyncTasks();
   }
 
@@ -79,12 +62,6 @@ class _DetectionState extends State<Detection> with SingleTickerProviderStateMix
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     var anxiety = globals.anxiety ?? "low";
     var depression = globals.depression ?? "low";
@@ -109,125 +86,272 @@ class _DetectionState extends State<Detection> with SingleTickerProviderStateMix
       ];
     }
 
+    // Anxiety Color and Text
+    Color anxietyColor;
+    String anxietyLevelText;
+    if (anxiety == 'low') {
+      anxietyColor = Colors.green;
+      anxietyLevelText = 'Low';
+    } else if (anxiety == 'medium') {
+      anxietyColor = Colors.orange;
+      anxietyLevelText = 'Medium';
+    } else {
+      anxietyColor = Colors.red;
+      anxietyLevelText = 'High';
+    }
+
+    // Depression Color and Text
+    Color depressionColor;
+    String depressionLevelText;
+    if (depression == 'low') {
+      depressionColor = Colors.green;
+      depressionLevelText = 'Low';
+    } else if (depression == 'medium') {
+      depressionColor = Colors.orange;
+      depressionLevelText = 'Medium';
+    } else {
+      depressionColor = Colors.red;
+      depressionLevelText = 'High';
+    }
+
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).padding.top + 20),
-
-              Text(
-                'SerenCoach',
-                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 36,
-                ),
+      body: Padding(
+        // padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.only(
+          top: 90, // Padding from the top
+          left: 30, // Padding from the left
+          right: 30, // Padding from the right
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // SerenCoach Title as Header
+            Center(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center, // Ensure horizontal centering
+                children: [
+                  ClipOval(
+                    child: Image.asset(
+                      'assets/images/therapist_avatar.png',
+                      width: 50, // Set the width of the image
+                      height: 50, // Set the height of the image
+                      fit: BoxFit.cover, // Ensure the image fills the circular area without distortion
+                    ),
+                  ), // Avatar
+                  const SizedBox(width: 10), // Space between the image and text
+                  Text(
+                    "SerenCoach",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.blueAccent,
+                    ),
+                  ), // SerenCoach
+                ],
               ),
-              SizedBox(height: 20),
+            ),
 
-              CircleAvatar(
-                radius: 125,
-                backgroundColor: Colors.transparent,
-                child: ClipOval(
-                  child: Image.asset(
-                    'assets/images/therapist_avatar.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
+            SizedBox(height: 20),
 
-              if (!_showActions) ...[
-                FadeTransition(
-                  opacity: _fadeAnimation,
+            // Prediction Display in One Card (Full Width)
+            Container(
+              width: double.infinity, // Ensure the card takes full width
+              child: Card(
+                color: Colors.white,  // Set background color to white
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'Here\'s what I\'ve observed from our conversation:',
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 20),
+                      // Anxiety Level Label and Result
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Anxiety Level:  ',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,  // Label color is black
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            anxietyLevelText,
+                            style: TextStyle(
+                              fontSize: 26, // Increased font size
+                              fontWeight: FontWeight.w600,
+                              color: anxietyColor, // Result color based on anxiety level
+                              fontStyle: FontStyle.italic, // Different font style for results
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Your anxiety level is $anxiety',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      Text(
-                        'Your depression level is $depression',
-                        style: TextStyle(fontSize: 18),
-                      ),
+
+                      SizedBox(height: 16),
+
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Depression Level:  ',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,  // Label color is black
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            depressionLevelText,
+                            style: TextStyle(
+                              fontSize: 26, // Increased font size
+                              fontWeight: FontWeight.w600,
+                              color: depressionColor, // Result color based on depression level
+                              fontStyle: FontStyle.italic, // Different font style for results
+                            ),
+                          ),
+                        ],
+                      )
+                      // Depression Level Label and Result
+
                     ],
                   ),
                 ),
-              ] else ...[
-                Text(
-                  'Check out these helpful therapies',
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 60),
+
+            // Helpful Therapies Section
+            // Helpful Therapies Section
+            Container(
+              width: double.infinity, // Ensures the container spans full width
+              decoration: BoxDecoration(
+                color: Colors.blue.shade50, // Background color for the section
+                borderRadius: BorderRadius.circular(16), // Rounded corners for the section
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3), // Subtle shadow
+                    spreadRadius: 3,
+                    blurRadius: 6,
+                    offset: Offset(0, 3), // Shadow position
                   ),
-                ),
-                SizedBox(height: 20),
-                Flexible(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: actions.length,
-                    itemBuilder: (context, index) {
-                      var action = actions[index];
-                      return Card(
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        child: ListTile(
-                            title: Text(
-                              action['title'] ?? '',
-                              style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 20),
-                            ),
-                            trailing: Icon(Icons.arrow_forward, color: Theme.of(context).colorScheme.primary),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Section Title
+                    Text(
+                      'Check out these helpful therapies:',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+
+                    SizedBox(height: 1),
+                    // List of Therapy Cards
+                    ListView.builder(
+                      shrinkWrap: true, // Ensures the ListView doesn't expand infinitely
+                      physics: NeverScrollableScrollPhysics(), // Prevents inner scrolling
+                      itemCount: actions.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.symmetric(vertical: 8),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          elevation: 4,
+                          child: InkWell(
                             onTap: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(
-                                  builder: (context) => action['widget'], // Use the widget defined in actions
-                                ),
+                                MaterialPageRoute(builder: (context) => actions[index]['widget']),
                               );
-                            }
-                        ),
-                      );
-                    },
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                              decoration: BoxDecoration(
+                                color: Colors.blue.shade100, // Background color of each card
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Row(
+                                children: [
+                                  // Icon or Placeholder (Optional)
+                                  CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor: Colors.blueAccent.withOpacity(0.3),
+                                    child: Icon(
+                                      Icons.favorite, // Replace with a suitable icon
+                                      color: Colors.blueAccent,
+                                    ),
+                                  ),
+                                  SizedBox(width: 16),
+
+                                  // Therapy Title
+                                  Expanded(
+                                    child: Text(
+                                      actions[index]['title'],
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black87,
+                                      ),
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios, // Navigation arrow
+                                    color: Colors.blueAccent,
+                                    size: 16,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // Go to Home Button
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerRight, // Align button to the right
+                child: ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pushNamed(context, '/home');
+                    await NotificationService.scheduleImmediateAndHourlyNotifications(widget.suggestedActivities);
+                  },
+                  child: Text(
+                    'Go to Home',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white, // Change font color to white
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
                   ),
                 ),
-                SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      await NotificationService.scheduleImmediateAndHourlyNotifications(widget.suggestedActivities);
-                      Navigator.pushReplacementNamed(context, '/home');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      'Go to Home',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ]
-            ],
-          ),
+              ),
+            ),
+
+          ],
         ),
       ),
     );
+
   }
 }
